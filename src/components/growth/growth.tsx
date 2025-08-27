3de59b43354ae6a0f13c9e111e5489bc2322dbf0
sophia vincent <sophire33@gmail.com>
2025-08-27 16:59:40 -0700

    still in progress

--- src/components/growth/growth.tsx
+++ src/components/growth/growth.tsx
@@ -2,19 +2,19 @@
 
 const Growth = () => {
     return ( 
-        <div className="mt-8 bg-[#262627] border-3 border-primary shadow-[0_0_10px_hsl(var(--color-primary))] w-90 md:w-60 h-30 lg:h-100 rounded-b-full rounded-r-full px-4 lg:px-8 py-2 lg:py-15 flex">
+        <div className="mt-8 bg-[#262627] border-3 border-secondary shadow-[0_0_10px_hsl(var(--color-primary))] w-90 md:w-60 h-30 lg:h-100 rounded-b-full rounded-r-full px-4 lg:px-8 py-2 lg:py-15 flex">
             <div className="flex md:flex-col flex-row justify-center items-center space-y-2 md:space-y-8">
                 <div className="flex items-center justify-center gap-5">
                     <span className="text-4xl md:text-6xl text-primary animate-fade-1">3</span>
-                    <p className="text-xs md:text-sm font-medium animate-fade-in">Programming Languages</p>
+                    <p className="text-xs md:text-sm font-medium animate-fade-in text-white">Programming Languages</p>
                 </div>
                 <div className="flex items-center justify-center gap-4">
                     <span className="text-4xl md:text-6xl text-primary animate-fade-2">4</span>
-                    <p className="text-xs md:text-sm font-medium animate-fade-in">Development Tools</p>
+                    <p className="text-xs md:text-sm font-medium animate-fade-in text-white">Development Tools</p>
                 </div>
                 <div className="flex items-center justify-center gap-4">
                     <span className="text-4xl md:text-6xl text-primary animate-fade-3">2</span>
-                    <p className="text-xs md:text-sm font-medium animate-fade-in">Years of Experience</p>
+                    <p className="text-xs md:text-sm font-medium animate-fade-in text-white">Years of Experience</p>
                 </div>
             </div>
         </div>
